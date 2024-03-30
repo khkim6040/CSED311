5958f2e9195656c613d67ec9dd39a5e4c4da90e9
Gwanho Kim <khkim6040@postech.ac.kr>
2024-03-30 17:34:28 +0900

    Add macro for opcodes and states

---
+++ lab3/states.v
@@ -0,0 +1,21 @@
+// Decleration states used in ControlUnit.v
+
+`define IF_1 4'b0000
+`define IF_2 4'b0001
+`define IF_3 4'b0010
+`define IF_4 4'b0011
+
+`define ID 4'b0100
+
+`define EX_1 4'b0101
+`define EX_2 4'b0110
+
+`define MEM_1 4'b0111
+`define MEM_2 4'b1000
+`define MEM_3 4'b1001
+`define MEM_4 4'b1010
+
+`define WB 4'b1011
+
+`define HALT 4'b1100
+
