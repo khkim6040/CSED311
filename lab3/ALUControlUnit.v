7e49f6152dbd8cba8d6daf7b70606ca10e84389e
Gwanho Kim <khkim6040@postech.ac.kr>
2024-03-30 18:32:59 +0900

    Write needed wires and rename

--- lab3/ALUControlUnit.v
+++ lab3/ALUControlUnit.v
@@ -4,12 +4,12 @@ module ALUControlUnit (
     input funct7,
     input[3:0] funct3,
     input[2:0] ALUOp,
-    output reg [3:0] alu_op
+    output reg [3:0] alu_ctrl_out
 );
 
     // TODO: Generate ALU control signal
     always @(*) begin 
-        
+
     end
 
 endmodule
--- lab3/cpu.v
+++ lab3/cpu.v
@@ -14,6 +14,32 @@ module cpu(input reset,       // positive reset signal
            output [31:0]print_reg[0:31]
            ); // Whether to finish simulation
   /***** Wire declarations *****/
+  wire [31:0] current_pc;
+  wire [31:0] next_pc;
+  wire [31:0] mem_src_mux_out;
+  wire [31:0] alu_src1_mux_out;
+  wire [31:0] alu_src2_mux_out;
+  wire [31:0] reg_write_mux_out;
+  wire [31:0] pc_src_mux_out;
+  wire [31:0] mem_dout;
+  wire [31:0] imm_gen_out;
+  wire [31:0] alu_out;
+  wire [31:0] alu_ctrl_out;
+  wire alu_bcond;
+ 
+  wire pc_write_cond;
+  wire pc_write;
+  wire i_or_d;
+  wire mem_read;
+  wire mem_write;
+  wire mem_to_reg;
+  wire IR_write;
+  wire pc_source;
+  wire [2:0] alu_op;
+  wire alu_srcA;
+  wire [2:0] alu_srcB;
+  wire reg_write;
+
 
   /***** Register declarations *****/
   reg [31:0] IR; // instruction register
@@ -21,7 +47,9 @@ module cpu(input reset,       // positive reset signal
   reg [31:0] A; // Read 1 data register
   reg [31:0] B; // Read 2 data register
   reg [31:0] ALUOut; // ALU output register
-  // Do not modify and use registers declared above.
+
+
+  
 
 
   
@@ -60,7 +88,7 @@ module cpu(input reset,       // positive reset signal
     .swch(),         // input
     .out()          // output
   );
-  
+
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
   PC pc(
@@ -92,7 +120,7 @@ module cpu(input reset,       // positive reset signal
     .din(),          // input
     .mem_read(),     // input
     .mem_write(),    // input
-    .dout()          // output
+    .mem_dout()          // output
   );
 
   // ---------- Control Unit ----------
@@ -121,7 +149,7 @@ module cpu(input reset,       // positive reset signal
     .funct7(),  // input
     .funct3(),  // input
     .ALUOp(),   // input
-    .alu_op()         // output
+    .alu_ctrl_out()         // output
   );
 
   // ---------- ALU ----------
@@ -129,7 +157,7 @@ module cpu(input reset,       // positive reset signal
     .alu_op(),      // input
     .alu_in_1(),    // input  
     .alu_in_2(),    // input
-    .alu_result(),  // output
+    .alu_out(),  // output
     .alu_bcond()     // output
   );
 
