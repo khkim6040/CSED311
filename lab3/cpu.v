a49a8e15499bea248e7500070cc101fb9c0f0599
khkim6040 <khkim6040@postech.ac.kr>
2024-03-31 14:01:20 +0900

    Revert cpu.v

--- lab3/cpu.v
+++ lab3/cpu.v
@@ -12,8 +12,34 @@ module cpu(input reset,       // positive reset signal
            input clk,         // clock signal
            output is_halted,
            output [31:0]print_reg[0:31]
-           ); // Whehther to finish simulation
+           ); // Whether to finish simulation
   /***** Wire declarations *****/
+  wire [31:0] current_pc;
+  wire [31:0] next_pc;
+  wire [31:0] mem_src_mux_out;
+  wire [31:0] alu_src1_mux_out;
+  wire [31:0] alu_src2_mux_out;
+  wire [31:0] reg_write_mux_out;
+  wire [31:0] pc_src_mux_out;
+  wire [31:0] mem_dout;
+  wire [31:0] imm_gen_out;
+  wire [31:0] alu_out;
+  wire [31:0] alu_ctrl_out;
+  wire alu_bcond;
+ 
+  wire pc_write_cond;
+  wire pc_write;
+  wire i_or_d;
+  wire mem_read;
+  wire mem_write;
+  wire mem_to_reg;
+  wire IR_write;
+  wire pc_source;
+  wire [2:0] alu_op;
+  wire alu_srcA;
+  wire [2:0] alu_srcB;
+  wire reg_write;
+
 
   /***** Register declarations *****/
   reg [31:0] IR; // instruction register
@@ -21,7 +47,47 @@ module cpu(input reset,       // positive reset signal
   reg [31:0] A; // Read 1 data register
   reg [31:0] B; // Read 2 data register
   reg [31:0] ALUOut; // ALU output register
-  // Do not modify and use registers declared above.
+
+
+  
+
+
+  
+
+  2mux mem_src_mux(
+    .x1(),        // input
+    .x2(),        // input
+    .swch(),         // input
+    .out()          // output
+  );
+
+  2mux alu_src1_mux(
+    .x1(),        // input
+    .x2(),        // input
+    .swch(),         // input
+    .out()          // output
+  );
+
+  2mux alu_src2_mux(
+    .x1(),        // input
+    .x2(),        // input
+    .swch(),         // input
+    .out()          // output
+  );
+
+  2mux reg_write_mux(
+    .x1(),        // input
+    .x2(),        // input
+    .swch(),         // input
+    .out()          // output
+  );
+
+  2mux pc_src_mux(
+    .x1(),        // input
+    .x2(),        // input
+    .swch(),         // input
+    .out()          // output
+  );
 
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
@@ -54,7 +120,7 @@ module cpu(input reset,       // positive reset signal
     .din(),          // input
     .mem_read(),     // input
     .mem_write(),    // input
-    .dout()          // output
+    .mem_dout()          // output
   );
 
   // ---------- Control Unit ----------
@@ -80,8 +146,10 @@ module cpu(input reset,       // positive reset signal
 
   // ---------- ALU Control Unit ----------
   ALUControlUnit alu_ctrl_unit(
-    .part_of_inst(),  // input
-    .alu_op()         // output
+    .funct7(),  // input
+    .funct3(),  // input
+    .ALUOp(),   // input
+    .alu_ctrl_out()         // output
   );
 
   // ---------- ALU ----------
@@ -89,7 +157,7 @@ module cpu(input reset,       // positive reset signal
     .alu_op(),      // input
     .alu_in_1(),    // input  
     .alu_in_2(),    // input
-    .alu_result(),  // output
+    .alu_out(),  // output
     .alu_bcond()     // output
   );
 
