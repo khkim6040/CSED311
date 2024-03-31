fd8c48af419b590ad46fd691d0a2d968761541db
khkim6040 <khkim6040@postech.ac.kr>
2024-03-31 13:58:11 +0900

    Change mux variable naming

--- lab3/2mux.v
+++ lab3/2mux.v
@@ -1,14 +1,14 @@
-module 2mux (input [31:0] x1,
-        input [31:0] x2,
+module 2mux (input [31:0] x0,
+        input [31:0] x1,
         input swch, 
         output reg [31:0] out);
 
     always @(*) begin
         if (swch == 1'b0) begin
-            out = x1;
+            out = x0;
         end
         else begin
-            out = x2;
+            out = x1;
         end
     end
 
--- lab3/4mux.v
+++ lab3/4mux.v
@@ -1,16 +1,16 @@
-module 4mux (input [31:0] x1,
+module 4mux (input [31:0] x0,
+        input [31:0] x1,
         input [31:0] x2,
         input [31:0] x3,
-        input [31:0] x4,
         input [1:0] swch, 
         output reg [31:0] out);
 
     always @(*) begin
         case (swch)
-            2'b00: out = x1;
-            2'b01: out = x2;
-            2'b10: out = x3;
-            2'b11: out = x4;
+            2'b00: out = x0;
+            2'b01: out = x1;
+            2'b10: out = x2;
+            2'b11: out = x3;
         endcase
     end
 
