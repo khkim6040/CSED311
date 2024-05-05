e8ea0a9cc7dccfdb8b18ddde79f0e285d49a5328
Gwanho Kim <khkim6040@postech.ac.kr>
2024-05-05 18:48:39 +0900

    Format

--- lab4-2/HazardDetector.v
+++ lab4-2/HazardDetector.v
@@ -31,7 +31,8 @@ module HazardDetector (input clk,
     // Branch uses both rs1, rs2
     // Jump uses only rs1
     wire is_rs1_used, is_rs2_used;
-    assign is_rs1_used = rs1 != 0 && (opcode == `ARITHMETIC || opcode == `ARITHMETIC_IMM || opcode == `LOAD || opcode == `STORE || opcode == `BRANCH || opcode == `JAL || opcode == `ECALL);
+    assign is_rs1_used = rs1 != 0 && (opcode == `ARITHMETIC || opcode == `ARITHMETIC_IMM || opcode == `LOAD || 
+                                        opcode == `STORE || opcode == `BRANCH || opcode == `JAL || opcode == `ECALL);
     assign is_rs2_used = rs2 != 0 && (opcode == `ARITHMETIC || opcode == `STORE || opcode == `BRANCH);
 
     // Detect Hazards
@@ -49,39 +50,33 @@ module HazardDetector (input clk,
             ID_EX_nop_signal = 1;
         end
         // Data Hazard: Ecall hazard because ecall comparison executes in ID stage
-        else if(is_ecall && (((is_rs1_used && rs1 == EX_rd) || (is_rs2_used && rs2 == EX_rd)) || ((is_rs1_used && rs1 == MEM_rd) || (is_rs2_used && rs2 == MEM_rd)))) begin
+        else if(is_ecall && (((is_rs1_used && rs1 == EX_rd) || (is_rs2_used && rs2 == EX_rd)) || 
+                ((is_rs1_used && rs1 == MEM_rd) || (is_rs2_used && rs2 == MEM_rd)))) begin
             // When hazard distance is 1
             PC_write = 0;
             IF_ID_write = 0;
             ID_EX_nop_signal = 1;
         end
         // Control Hazard: Branch hazard
-        else if(bcond == `BCOND_TAKEN && ID_PC == target_pc) begin
-            // Correct PC prediction, nothing to do
-        end
-        else if(bcond == `BCOND_NOT_TAKEN && ID_PC == EX_PC+4) begin
-            // Correct PC prediction, nothing to do
-        end
         else if(bcond == `BCOND_TAKEN && ID_PC != target_pc) begin
             IF_ID_nop_signal = 1;
             ID_EX_nop_signal = 1;
             EX_correct_next_pc = target_pc;
             EX_PCSrc = 1;
         end
+        // Control Hazard: Branch hazard
         else if(bcond == `BCOND_NOT_TAKEN && ID_PC != EX_PC+4) begin
             IF_ID_nop_signal = 1;
             ID_EX_nop_signal = 1;
             EX_correct_next_pc = EX_PC+4;
             EX_PCSrc = 1;
         end
+        // Control Hazard: Jump hazard
         else if(bcond == `BCOND_JUMP && ID_PC != target_pc) begin
             IF_ID_nop_signal = 1;
             ID_EX_nop_signal = 1;
             EX_correct_next_pc = target_pc;
             EX_PCSrc = 1;
-        end
-        else if(bcond == `BCOND_JUMP && ID_PC == target_pc) begin
-            // Correct PC prediction, nothing to do
         end
         else begin
             PC_write = 1;
